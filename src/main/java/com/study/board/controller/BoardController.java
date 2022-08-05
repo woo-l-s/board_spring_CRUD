09c5b375fadcacc669ebeff8be14e05b6b2f55ab
jin <tjrdn110@naver.com>
2022-08-05 19:25:57 +0900

    My Board

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,80 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String boardwriteForm() {
+
+        return "boardwrite";
+    }
+
+
+    @PostMapping("/board/writepro")
+    public String boardwritePro(Board board, Model model, MultipartFile file) throws Exception{
+
+        boardService.write(board, file);
+
+        model.addAttribute("message", "글 작성 완료!");
+        model.addAttribute("searchUrl", "/board/list");
+        return "message";
+    }
+
+    @GetMapping("/board/list")
+    public String boardlist(Model model){
+        model.addAttribute("list", boardService.boardList());
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardview(Model model, Integer id){
+        System.out.println(id);
+        model.addAttribute("board", boardService.boardview(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        System.out.println("de");
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model){
+
+        model.addAttribute("board", boardService.boardview(id));
+        return "boardmodify";
+
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception{
+        Board boardTemp = boardService.boardview(id);
+
+        System.out.println(boardTemp.getId());
+        boardTemp.setTitle(board.getTitle());
+//        System.out.println(board.getTitle());
+//       System.out.println("a2");
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp, file);
+        return "redirect:/board/list";
+
+    }
+
+}
---
+++ src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,53 @@
+package com.study.board.service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+    //글 작성
+    public void write(Board board, MultipartFile file) throws Exception{
+
+        String projectPath = System.getProperty("user.dir")+ "\\src\\main\\resources\\static\\files";
+
+        UUID uuid = UUID.randomUUID();
+
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath, fileName);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+
+        board.setFilepath("/files/"+ fileName);
+
+        boardRepository.save(board);
+    }
+    //게시글  리스트 처리
+    public List<Board> boardList(){
+        return boardRepository.findAll();
+    }
+
+    //특정 게시물 검색
+    public Board boardview(Integer id){
+//      Optional 값으로 받아오기 때문에 get()
+        return boardRepository.findById(id).get();
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id){
+
+        boardRepository.deleteById(id);
+    }
+}
